56dd5bcc55ef2b97bd9841670299554ca709b4a8
iza lumpio <iza.lumpio@edu.sait.ca>
2023-02-10 14:10:57 -0700

    addedan interface

---
+++ AnimalLab/IAnimal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalLab
+{
+    public interface IAnimal
+    {
+        string Name { get; set; }   
+        string Colour { get; set; }
+        int Age { get; set; }
+
+        //eat method -  void
+        //note: to be implemented on the implementation class
+
+        //cry method - string
+        //note: to be implemented on the implementation class
+
+
+
+
+    }
+}
